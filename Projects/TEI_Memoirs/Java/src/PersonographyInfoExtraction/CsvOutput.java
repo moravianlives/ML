81d253e04e22b6aed08e9047c943694266b2d8df
Justin Schaumberger <jes083@bucknell.edu>
2020-05-27 09:10:01 -0400

    Commit

---
+++ Projects/TEI_Memoirs/Java/src/PersonographyInfoExtraction/CsvOutput.java
@@ -0,0 +1,9 @@
+package PersonographyInfoExtraction;
+
+/**
+ * Name: Justin Schaumberger
+ * File: CsvOutput.java
+ * Date: 5/21/20
+ */
+public class CsvOutput {
+}
---
+++ Projects/TEI_Memoirs/Java/src/PersonographyInfoExtraction/Extract.java
@@ -0,0 +1,9 @@
+package PersonographyInfoExtraction;
+
+/**
+ * Name: Justin Schaumberger
+ * File: extract.java
+ * Date: 5/21/20
+ */
+public class extract {
+}

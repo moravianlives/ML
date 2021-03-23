a23a26264b519d928f20d7ade3bf2c281a34a55a
Luyang Ren <lr030@bucknell.edu>
2021-03-23 16:48:59 -0400

    Add Zen to the project

---
+++ zen/Zen.Media/Processing/Pipeline/RasterImagePipeline.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+
+namespace Zen.Media.Processing.Pipeline
+{
+    public class RasterImagePipeline
+    {
+        public List<IRasterImagePipelineItem> Items = new List<IRasterImagePipelineItem>();
+        public Stream SourceStream { get; set; }
+        public Image SourceImage { get; set; }
+        public ImagePackage SourcePackage { get; set; }
+        public IImageFormat Format { get; set; }
+
+        public Stream Process()
+        {
+            if (SourceStream!= null)
+                if (SourceImage == null)
+                {
+                    SourceImage = Image.Load(SourceStream, out var format);
+                    Format = format;
+                }
+
+            if (SourceImage!= null)
+                if (SourcePackage == null)
+                    SourcePackage = new ImagePackage { Format = Format, Image = SourceImage };
+
+            // Now, backfills.
+
+            SourceImage = SourceImage ?? SourcePackage.Image;
+            Format = Format ?? SourcePackage.Format;
+
+            foreach (var item in Items) SourceImage = item.Process(SourceImage);
+
+            var memoryStream = new MemoryStream();
+            SourceImage.Save(memoryStream, Format);
+
+            memoryStream.Position = 0;
+
+            return memoryStream;
+        }
+    }
+}
---
+++ zen/Zen.Storage/Model/ZenFileBaseDescriptor.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Zen.Base.Extension;
+using Zen.Base.Module;
+using Zen.Storage.Provider.File;
+
+namespace Zen.Storage.Model
+{
+    public class ZenFileBaseDescriptor<T> : Data<T> where T : Data<T>
+    {
+        private readonly ZenFileDescriptorAttribute _attribute;
+        private readonly FileStoragePrimitive _provider;
+
+        public ZenFileBaseDescriptor()
+        {
+            _attribute = GetType()
+                             .GetCustomAttributes(typeof(ZenFileDescriptorAttribute), false)
+                             .Select(i => (ZenFileDescriptorAttribute) i)
+                             .FirstOrDefault() ?? new ZenFileDescriptorAttribute();
+            _provider =
+                _attribute.StorageType == null ? Current.FileStorageProvider : _attribute.StorageType.CreateInstance<FileStoragePrimitive>();
+        }
+
+        public async Task<Stream> Fetch() { return await _provider.Fetch((IFileDescriptor) this); }
+
+        public async Task<string> Store(Stream source) { return await _provider.Store((IFileDescriptor) this, source); }
+        public async Task<bool> Exists() { return await _provider.Exists((IFileDescriptor) this); }
+    }
+}

71e828b20b3ea776bee89ae9b67f7ae491292c87
Daniel  Mancusi <dmancusi@bucknell.edu>
2021-07-09 10:48:43 -0400

    incorporate edu.bucknell.framework in ML repo

--- zen/Zen.Web/obj/Debug/netcoreapp3.1/Zen.Web.Version.cs
+++ zen/Zen.Web/obj/Debug/netcoreapp3.1/Zen.Web.Version.cs
@@ -9,8 +9,8 @@
 //------------------------------------------------------------------------------
 
 [assembly: System.Reflection.AssemblyVersionAttribute("0.6.0.0")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("0.6.0.70")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.6.0-alpha+8231430028")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("0.6.0.72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.6.0-alpha+a3b2032c0d")]
 #if NETSTANDARD || NETFRAMEWORK || NETCOREAPP
 [System.CodeDom.Compiler.GeneratedCode("Nerdbank.GitVersioning.Tasks","3.3.37.35081")]
 #endif
@@ -19,14 +19,14 @@
 #endif
 internal static partial class ThisAssembly {
     internal const string AssemblyVersion = "0.6.0.0";
-    internal const string AssemblyFileVersion = "0.6.0.70";
-    internal const string AssemblyInformationalVersion = "0.6.0-alpha+8231430028";
+    internal const string AssemblyFileVersion = "0.6.0.72";
+    internal const string AssemblyInformationalVersion = "0.6.0-alpha+a3b2032c0d";
     internal const string AssemblyName = "Zen.Web";
     internal const string AssemblyTitle = "Zen.Web";
     internal const string AssemblyConfiguration = "Debug";
-    internal const string GitCommitId = "823143002833212a3cc00ec3b7bdab8ef68b6303";
+    internal const string GitCommitId = "a3b2032c0da1130562e0831b0edc559ea1935f19";
     internal const bool IsPublicRelease = false;
     internal const bool IsPrerelease = true;
-    internal static readonly System.DateTime GitCommitDate = new System.DateTime(637607444810000000L, System.DateTimeKind.Utc);
+    internal static readonly System.DateTime GitCommitDate = new System.DateTime(637612790920000000L, System.DateTimeKind.Utc);
     internal const string RootNamespace = "Zen.Web";
 }

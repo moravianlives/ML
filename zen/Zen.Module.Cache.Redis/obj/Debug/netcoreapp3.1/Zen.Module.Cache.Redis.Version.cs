572d82fe889d8a6a230b7f868ca5013dd6e92e64
Luyang  Ren <lr030@bucknell.edu>
2021-10-21 11:46:21 -0400

    pull new data, like relations, from xml files

--- zen/Zen.Module.Cache.Redis/obj/Debug/netcoreapp3.1/Zen.Module.Cache.Redis.Version.cs
+++ zen/Zen.Module.Cache.Redis/obj/Debug/netcoreapp3.1/Zen.Module.Cache.Redis.Version.cs
@@ -9,8 +9,8 @@
 //------------------------------------------------------------------------------
 
 [assembly: System.Reflection.AssemblyVersionAttribute("0.6.0.0")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("0.6.0.107")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.6.0-alpha+1360881148")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("0.6.0.117")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.6.0-alpha+9f5dfa0cdf")]
 #if NETSTANDARD || NETFRAMEWORK || NETCOREAPP
 [System.CodeDom.Compiler.GeneratedCode("Nerdbank.GitVersioning.Tasks","3.3.37.35081")]
 #endif
@@ -19,14 +19,14 @@
 #endif
 internal static partial class ThisAssembly {
     internal const string AssemblyVersion = "0.6.0.0";
-    internal const string AssemblyFileVersion = "0.6.0.107";
-    internal const string AssemblyInformationalVersion = "0.6.0-alpha+1360881148";
+    internal const string AssemblyFileVersion = "0.6.0.117";
+    internal const string AssemblyInformationalVersion = "0.6.0-alpha+9f5dfa0cdf";
     internal const string AssemblyName = "Zen.Module.Cache.Redis";
     internal const string AssemblyTitle = "Zen.Module.Cache.Redis";
     internal const string AssemblyConfiguration = "Debug";
-    internal const string GitCommitId = "1360881148693cb82f485b6a98c6732561bceed1";
+    internal const string GitCommitId = "9f5dfa0cdf1c291b9d3346757aad3a0dbf76dd86";
     internal const bool IsPublicRelease = false;
     internal const bool IsPrerelease = true;
-    internal static readonly System.DateTime GitCommitDate = new System.DateTime(637687114020000000L, System.DateTimeKind.Utc);
+    internal static readonly System.DateTime GitCommitDate = new System.DateTime(637702477900000000L, System.DateTimeKind.Utc);
     internal const string RootNamespace = "Zen.Module.Cache.Redis";
 }
